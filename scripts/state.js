d898659753b88ff168476ed0c1cef9eed011eb3e
IvanArtukhevych <ivan.dolbilow@gmail.com>
2022-11-07 19:47:12 +0300

    first commit

---
+++ scripts/state.js
@@ -0,0 +1,11 @@
+var c = document.getElementById('cnv')
+var ctx = c.getContext('2d')
+const width = c.width = window.innerWidth;
+const height = c.height = window.innerHeight;
+
+var state = []
+var jews = []
+var shoots = []
+var rotate = 0
+
+console.log('state is loaded');
